fc186fbfcaeb23275b28a9935f1518150ea8249e
Aiesha818 <81634184+Aiesha818@users.noreply.github.com>
2021-04-19 13:51:44 +0530

    Add files via upload

---
+++ replace.java
@@ -0,0 +1,9 @@
+public class replace
+{
+public static void main(String a[])
+{
+String str=new String("summer");
+System.out.print("String after replacing all 'm' with 't':");
+System.out.println(str.replace('m','t'));
+}
+}
