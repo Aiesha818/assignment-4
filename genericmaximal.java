adac4c09487a595bb548661b4781525223fd833f
Aiesha818 <81634184+Aiesha818@users.noreply.github.com>
2021-04-22 12:13:40 +0530

    Add files via upload

---
+++ genericmaximal.java
@@ -0,0 +1,12 @@
+public final class genericmaximal {
+    public static <T extends Object & Comparable<? super T>>
+        T max(List<? extends T> list, int begin, int end) {
+
+        T maxElem = list.get(begin);
+
+        for (++begin; begin < end; ++begin)
+            if (maxElem.compareTo(list.get(begin)) < 0)
+                maxElem = list.get(begin);
+        return maxElem;
+    }
+}
